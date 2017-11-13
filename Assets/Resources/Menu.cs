f48514604363669e11cb9cff6e332824e8e29a5b
oludom <mhlauffen@web.de>
2017-11-13 11:41:22 +0100

    initial

---
+++ Assets/Resources/Menu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour {
+
+    public void StartCam()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
